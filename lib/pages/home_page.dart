e0e34232f37344c2877f0298fcdfceb05612a34c
beeswa <bishwadeepc72@gmaiL.com>
2022-01-14 00:10:19 +0545

    day15 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -18,26 +18,36 @@ class _HomePageState extends State<HomePage> {
   }
 
   dataLoad() async {
+    await Future.delayed(const Duration(seconds: 2));
     final catalogJson =
         await rootBundle.loadString('assets/files/catalog.json');
     final decodedData = jsonDecode(catalogJson);
     var productData = decodedData['products'];
+    CatalogModel.items =
+        List.from(productData).map<Item>((item) => Item.fromMap(item)).toList();
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(5, (index) => CatalogModel.items[0]);
+    // final dummyList = List.generate(5, (index) => CatalogModel.items[0]);
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Catalog App'),
+        title: const Text('Catalog App'),
       ),
-      body: ListView.builder(
-          itemCount: dummyList.length,
-          itemBuilder: (context, index) {
-            return ItemWidget(item: dummyList[index]);
-          }),
-      drawer: MyDrawer(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: (CatalogModel.items.isNotEmpty)
+            ? ListView.builder(
+                itemCount: CatalogModel.items.length,
+                itemBuilder: (context, index) =>
+                    ItemWidget(item: CatalogModel.items[index]))
+            : const Center(
+                child: CircularProgressIndicator(),
+              ),
+      ),
+      drawer: const MyDrawer(),
     );
   }
 }
