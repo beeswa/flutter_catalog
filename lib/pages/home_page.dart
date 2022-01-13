ad3ee0bbf39260366c49a81a560f83258a3881cd
beeswa <bishwadeepc72@gmaiL.com>
2022-01-13 21:46:35 +0545

    day13 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
 import 'package:flutter_catalog/widgets/item_widget.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    dataLoad();
+  }
+
+  dataLoad() async {
+    final catalogJson =
+        await rootBundle.loadString('assets/files/catalog.json');
+    final decodedData = jsonDecode(catalogJson);
+    var productData = decodedData['products'];
+  }
+
   @override
   Widget build(BuildContext context) {
-    int days = 30;
-    var name = 'Bishwadeep';
     final dummyList = List.generate(5, (index) => CatalogModel.items[0]);
 
     return Scaffold(
